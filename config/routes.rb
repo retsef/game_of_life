d58d93b5a278e333664dcf70a152f1f6ac877e9e
Roberto Scinocca <roberto.scinocca@gmail.com>
2022-09-16 12:56:03 +0200

    Add Devise for authentication as app request specify

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  draw :auth
+
+  resources :simulations, except: %i[edit update] do
+    patch :start, on: :member
+    patch :pause, on: :member
+
+    resources :generations, only: %i[index show]
+  end
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root controller: 'auth/user_sessions', action: :new
+  root controller: :simulations, action: :new
+
+  draw :errors
 end
---
+++ config/routes/auth.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users,
+             path: 'auth',
+             controllers: {
+               sessions: 'auth/sessions',
+               registrations: 'auth/registrations',
+               passwords: 'auth/passwords'
+             }
+end
