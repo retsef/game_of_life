e609fb1cf8abc0c76b3fb17e155153b5032a9b4f
Roberto Scinocca <roberto.scinocca@gmail.com>
2022-09-19 06:16:08 +0200

    add logic for mobile sidebar interaction

--- app/javascript/controllers/navigation_sidebar_controller.js
+++ app/javascript/controllers/navigation_sidebar_controller.js
@@ -1,17 +1,46 @@
 import ApplicationController from 'controllers/application_controller';
 
 export default class extends ApplicationController {
-  static targets = ['button', 'panel'];
+  static targets = ['button', 'sidebar', 'button_icon_closed', 'button_icon_opened'];
 
   toggle() {
-    this.panelTarget.classList.toggle('hidden');
+    this.sidebarTarget.classList.toggle('hidden');
+    this.setIconBySidebar();
   }
 
   show() {
-    this.removeClassIfExist(this.panelTarget, 'hidden');
+    this.removeClassIfExist(this.sidebarTarget, 'hidden');
+    this.setIconBySidebar();
   }
 
   hide() {
-    this.addClassUnlessExist(this.panelTarget, 'hidden');
+    this.addClassUnlessExist(this.sidebarTarget, 'hidden');
+    this.setIconBySidebar();
+  }
+
+  setIconBySidebar() {
+    if(this.sidebarTarget.classList.contains('hidden')) {
+      console.log('show icon opened');
+      this.showIconOpened();
+    } else {
+      console.log('show icon closed');
+      this.showIconClosed();
+    }
+  }
+
+  showIconOpened() {
+    this.button_icon_closedTarget.classList.add('hidden');
+    this.button_icon_closedTarget.classList.remove('block');
+
+    this.button_icon_openedTarget.classList.remove('hidden');
+    this.button_icon_openedTarget.classList.add('block');
+  }
+
+  showIconClosed() {
+    this.button_icon_closedTarget.classList.remove('hidden');
+    this.button_icon_closedTarget.classList.add('block');
+
+    this.button_icon_openedTarget.classList.add('hidden');
+    this.button_icon_openedTarget.classList.remove('block');
   }
 }
